7e8b5ff8592686e4082b063544e58f608ce332fb
Didier Hategekimana <kdondidier@gmail.com>
2024-01-05 21:57:08 +1100

    Update metadata and social links

--- components/Social.tsx
+++ components/Social.tsx
@@ -7,15 +7,21 @@ export default function Social() {
         <a
           className="social_link linkedin"
           href="https://www.linkedin.com/in/didier-h-66953a211/"
+          aria-label="check out my LinkedIn profile"
         >
           <Image src="/linkedin.png" width="24" height="24" alt="" />
         </a>
-        <a className="social_link github" href="https://github.com/Didier187">
+        <a
+          className="social_link github"
+          href="https://github.com/Didier187"
+          aria-label="check out my github profile"
+        >
           <Image src="/github-sign.png" width="24" height="24" alt="" />
         </a>
         <a
           className="social_link twitter"
           href="https://twitter.com/KAMANUTSID"
+          aria-label="check out my twitter profile"
         >
           <Image src="/twitter.png" width="24" height="24" alt="" />
         </a>
