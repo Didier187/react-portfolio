c7a9621c3462a0dbd195b183607938eb3228d2d4
Didier Hategekimana <kdondidier@gmail.com>
2024-01-11 22:28:52 +1100

    Refactor getStaticPaths function in page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,25 +1,13 @@
 import { readFile, readdir } from "fs/promises";
 import matter from "gray-matter";
-import { GetStaticPaths } from "next";
 
-type Post = {
-  data: {
-    title: string;
-  };
-  content: string;
-};
-
-const getStaticPaths = (async () => {
+export const generateStaticParams = async () => {
   const entries = await readdir("./public/posts", { withFileTypes: true });
 
-  return {
-    paths: entries.map((dir) => ({
-      params: { slug: dir.name.split(".")[0].toString() },
-    })),
-    fallback: true,
-  };
-}) satisfies GetStaticPaths;
-
+  return entries.map((post) => ({
+    slug: post.name.split(".")[0].toString(),
+  }));
+};
 
 export default async function page({ params }) {
   const fileContent = await readFile(
