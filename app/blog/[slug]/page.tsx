082cff49ae18acfc409eb57e4b4adb33ba97d212
Didier Hategekimana <kdondidier@gmail.com>
2024-01-12 17:03:20 +1100

    Delete unnecessary files and update blog styles

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,11 @@
 import { readFile, readdir } from "fs/promises";
 import matter from "gray-matter";
+// testing rehype ecosystem
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypePrettyCode from "rehype-pretty-code";
 
 export const generateStaticParams = async () => {
   const entries = await readdir("./public/posts", { withFileTypes: true });
@@ -11,17 +17,30 @@ export const generateStaticParams = async () => {
 
 export default async function page({ params }) {
   const fileContent = await readFile(
-    `./public/posts/${params.slug}.md`,
+    `./public/posts/${params.slug}.mdx`,
     "utf8"
   );
   const { data, content } = matter(fileContent);
-
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypePrettyCode, {
+      // See Options section below.
+    })
+    .use(rehypeStringify)
+    .process(content);
   return (
     <div>
       <h1>This is a blog post</h1>
       {data.title}
       <br />
-      {content}
+      <div className="blog-content">
+        <div
+          dangerouslySetInnerHTML={{
+            __html: String(file),
+          }}
+        />
+      </div>
     </div>
   );
 }
